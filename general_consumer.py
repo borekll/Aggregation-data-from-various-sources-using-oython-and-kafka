947f1c4fed89e02d7d88ba344f8051b0c21bee09
Luna <73404617+Stefan-Luna-Domaradzki@users.noreply.github.com>
2022-12-05 13:09:21 +0100

    updated general consumer

--- general_consumer.py
+++ general_consumer.py
@@ -1,4 +1,5 @@
 from kafka import KafkaConsumer
+
 import json
 
 
@@ -7,21 +8,21 @@ traffic_consumer = KafkaConsumer(auto_offset_reset='earliest', bootstrap_servers
 traffic_consumer.subscribe("traffic")
 print("zasubskrybowano do traffic")
 
-email_consumer = KafkaConsumer(auto_offset_reset='earliest', bootstrap_servers=['localhost:9092'],
-                               api_version=(0, 10))
 
-email_consumer.subscribe('emails')
-print("zasubskrybowano do emails")
-
+traffic_messages=0
 
 for traffic in traffic_consumer:
 
+    traffic_messages+=1
     print("traffic loop")
 
     value = traffic.value
     value = json.loads(traffic.value)
 
     print(value)
+
+    if traffic_messages == 50:
+        break
 
    #bledy w consumerze zmusily do usuniecia pobierania
    #do chwili naprawienia tylko wyswietlanie
@@ -30,11 +31,21 @@ for traffic in traffic_consumer:
 #-- emaile
 
 
-message_count = 1
-data_user_id = []
-data_recipient_id = []
-data_message = []
-data_date = []
+#message_count = 1
+#data_user_id = []
+#data_recipient_id = []
+#data_message = []
+#data_date = []
+
+print("emails consumer")
+
+email_consumer = KafkaConsumer(auto_offset_reset='earliest', bootstrap_servers=['localhost:9092'],
+                               api_version=(0, 10))
+
+email_consumer.subscribe('emails')
+print("zasubskrybowano do emails")
+
+emails_messages = 0
 
 for emails in email_consumer:
 
@@ -45,15 +56,24 @@ for emails in email_consumer:
 
     print(value)
 
+
+    #print(value)
+
+    if emails_messages == 50:
+        break
+
     user_id = int(value['user_id'])
     recipient_id = int(value['recipient_id'])
     message = str(value['message'])
     date = int(value['date'])
 
+
+
     #print(date)
-    data_user_id.append(user_id)
-    data_recipient_id.append(recipient_id)
-    data_message.append(message)
-    data_date.append(date)
-    message_count += 1
+    #data_user_id.append(user_id)
+    #data_recipient_id.append(recipient_id)
+    #data_message.append(message)
+    #data_date.append(date)
+    #message_count += 1
+
 
